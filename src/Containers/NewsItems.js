da2b4d4cbf87ca4b8c71cff5c12400ea273b96bd
Ankit Jain <ankit9252984512@gmail.com>
2023-07-06 16:30:19 +0530

    Lecture 25, class states, making article array

--- src/Containers/NewsItems.js
+++ src/Containers/NewsItems.js
@@ -5,10 +5,12 @@ import React, { Component } from 'react'
 
 //  So we will show the title and description in the news items along with their images
 export class NewsItems extends Component {
+
+
     render() {
-        let {title, discreption} = this.props;
+        let {title, description , imgUrl, newsUrl } = this.props;
         // Using destructuring here. Title and description will get there respective value from the this.props object. We can use title and description directly now insetead of using this.props.title and this.props.description
-        
+
         return (
             <div>
                 News Items
@@ -16,11 +18,12 @@ export class NewsItems extends Component {
 {/* Making card from bootstrap here */}
 
                 <div className="card" style={{width: "18rem"}}>
-                    <img className="card-img-top" src="..." alt="Card image cap"/>
+                    <img className="card-img-top" src={imgUrl} alt="Card image cap"/>
                         <div className="card-body">
-                            <h5 className="card-title">title</h5>
-                            <p className="card-text">discreption</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <h5 className="card-title">{title}</h5>
+                            <p className="card-text">{description}</p>
+                            <a href={newsUrl} className="btn btn-primary btn-sm">Read More</a>
+                            {/* Giving btn-sm class here to make the button smaller */}
                         </div>
                 </div>
             </div>
