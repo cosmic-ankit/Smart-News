40017e5242c78eacf44b945678de46fa47fa9eab
Ankit Jain <ankit9252984512@gmail.com>
2023-07-07 13:32:30 +0530

    Lecture 26 Adding Map functions and limiting the size of title and description

--- src/Containers/NewsItems.js
+++ src/Containers/NewsItems.js
@@ -8,7 +8,7 @@ export class NewsItems extends Component {
 
 
     render() {
-        let {title, description , imgUrl, newsUrl } = this.props;
+        let {title, description , newsUrl, imgUrl } = this.props;
         // Using destructuring here. Title and description will get there respective value from the this.props object. We can use title and description directly now insetead of using this.props.title and this.props.description
 
         return (
@@ -20,9 +20,9 @@ export class NewsItems extends Component {
                 <div className="card" style={{width: "18rem"}}>
                     <img className="card-img-top" src={imgUrl} alt="Card image cap"/>
                         <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <a href={newsUrl} className="btn btn-primary btn-sm">Read More</a>
+                            <h5 className="card-title">{title} </h5>
+                            <p className="card-text">{description} ...</p>
+                            <a href={newsUrl}  target = "_blank" className="btn btn-primary btn-sm">Read More</a>
                             {/* Giving btn-sm class here to make the button smaller */}
                         </div>
                 </div>
@@ -32,3 +32,5 @@ export class NewsItems extends Component {
 }
 
 export default NewsItems
+
+
